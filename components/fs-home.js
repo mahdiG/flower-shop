cef0f2ad6ffe04b123101f7a08a4afa14556533e
Mahdi <mahdi.gerami77@gmail.com>
2019-08-10 23:11:21 +0430

    Create grid for cards.

--- components/fs-home.js
+++ components/fs-home.js
@@ -21,6 +21,38 @@ class Home extends LitElement {
           " شتخسی بشسیب شس یب شس یب شسی بش سی بش سیب ش سی ب شس یبشسیبگل آفتابگردووووووووووووووووووووووووووووووووووووووووون",
         price: "۱۲۰۰۰۰",
         url: "/assets/images/sunflower.jpg"
+      },
+      {
+        name: "گل سفید",
+        price: "۲۰۰۰۰",
+        url: "/assets/images/whiteflower.jpg"
+      },
+      {
+        name: "گل صورتی",
+        price: "۴۰۰۰۰",
+        url: "/assets/images/pinkflower.jpg"
+      },
+      {
+        name:
+          " شتخسی بشسیب شس یب شس یب شسی بش سی بش سیب ش سی ب شس یبشسیبگل آفتابگردووووووووووووووووووووووووووووووووووووووووون",
+        price: "۱۲۰۰۰۰",
+        url: "/assets/images/sunflower.jpg"
+      },
+      {
+        name: "گل سفید",
+        price: "۲۰۰۰۰",
+        url: "/assets/images/whiteflower.jpg"
+      },
+      {
+        name: "گل صورتی",
+        price: "۴۰۰۰۰",
+        url: "/assets/images/pinkflower.jpg"
+      },
+      {
+        name:
+          " شتخسی بشسیب شس یب شس یب شسی بش سی بش سیب ش سی ب شس یبشسیبگل آفتابگردووووووووووووووووووووووووووووووووووووووووون",
+        price: "۱۲۰۰۰۰",
+        url: "/assets/images/sunflower.jpg"
       }
     ];
   }
@@ -38,15 +70,47 @@ class Home extends LitElement {
       .home {
         margin: 1rem;
       }
+      .grid-container {
+        display: grid;
+        grid-template-columns: repeat(5, auto);
+        grid-row-gap: 3rem;
+        justify-content: space-around;
+      }
+
+      /* Extra small devices (phones, 600px and down) */
+      @media only screen and (max-width: 600px) {
+        .grid-container {
+          grid-template-columns: repeat(1, auto);
+          justify-content: center;
+        }
+      }
+
+      /* Small devices (portrait tablets and large phones, 600px and up) */
+      @media only screen and (min-width: 600px) {
+        .grid-container {
+          grid-template-columns: repeat(2, auto);
+          justify-content: space-around;
+        }
+      }
+
+      /* Large devices (laptops/desktops, 992px and up) */
+      @media only screen and (min-width: 992px) {
+        .grid-container {
+          display: grid;
+          grid-template-columns: repeat(5, auto);
+          /* grid-row-gap: 3rem; */
+        }
+      }
     `;
   }
 
   render() {
     return html`
-      <div class="home">
+      <div class="grid-container home">
         ${this.images.map(image => {
           return html`
             <fs-card
+              class="grid-item"
               name=${image.name}
               price="${image.price}"
               url="${image.url}"
